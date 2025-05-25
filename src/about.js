b46156b576c5c1815fd689214de3a1c94fe324a8
„benearne“ <b.arnold@posteo.de>
2025-05-25 17:25:04 +0200

    set up final css

--- src/about.js
+++ src/about.js
@@ -1,3 +1,5 @@
+import restImg from "./restaurant.jpg";
+
 export function setAbout () {
     const content = document.getElementById("content");
 
@@ -11,4 +13,9 @@ export function setAbout () {
     const historyRestaurant = document.createElement("p");
     historyRestaurant.textContent = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
     content.appendChild(historyRestaurant);
+
+    const restaurantPicture = document.createElement("img");
+    restaurantPicture.src = restImg
+    restaurantPicture.alt = "restaurant";
+    content.appendChild(restaurantPicture)
 }
